0864711f92ffaaafeb41a1311a6d2c5e2e27953d
Gustavo Franca <gustavocandidofranca@gmail.com>
2021-08-09 21:26:51 -0300

    adiciona navegação a página de cadastro

---
+++ app/src/main/java/com/example/helloworld/DisplayRegisterActivity.java
@@ -0,0 +1,15 @@
+package com.example.helloworld;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DisplayRegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro);
+
+    }
+}
