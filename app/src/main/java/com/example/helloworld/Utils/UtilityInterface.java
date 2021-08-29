3ba98ee24c4066c91808db271ce339f1cd1ad583
Gustavo <gustavocandidofranca@gmail.com>
2021-08-28 22:16:23 -0300

    add logout,feed and upload post feature

--- app/src/main/java/com/example/helloworld/Utils/UtilityInterface.java
+++
@@ -1,6 +0,0 @@
-package com.example.helloworld.Utils;
-
-public interface UtilityInterface {
-
-    void loadMore(long limit);
-}
--- app/src/main/java/com/example/helloworld/common/feed/FeedActivity.java
+++ app/src/main/java/com/example/helloworld/common/feed/FeedActivity.java
@@ -1,41 +1,135 @@
 package com.example.helloworld.common.feed;
 
-import android.os.Bundle;
-
-import com.google.android.material.appbar.CollapsingToolbarLayout;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
+import com.example.helloworld.MainActivity;
+import com.example.helloworld.UploadActivity;
+import com.example.helloworld.adapter.FeedRecyclerAdapter;
 import com.example.helloworld.databinding.ActivityFeedBinding;
+import com.example.helloworld.model.Post;
+import com.example.helloworld.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.Map;
 
 public class FeedActivity extends AppCompatActivity {
 
+    private FirebaseAuth firebaseAuth;
+    private FirebaseFirestore firebaseFirestore;
+    ArrayList<Post> postArrayList;
+    FeedRecyclerAdapter feedRecyclerAdapter;
     private ActivityFeedBinding binding;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.insta_options_menu,menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if (item.getItemId() == R.id.add_post) {
+            Intent intentToSignUp = new Intent(FeedActivity.this, UploadActivity.class);
+            startActivity(intentToSignUp);
+        } else if (item.getItemId() == R.id.signout) {
+
+            firebaseAuth.signOut();
+
+            Intent intentToSignUp = new Intent(FeedActivity.this, MainActivity.class);
+            startActivity(intentToSignUp);
+            finish();
+        }
+
+
+
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         binding = ActivityFeedBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        View view = binding.getRoot();
+        setContentView(view);
 
-        Toolbar toolbar = binding.toolbar;
-        setSupportActionBar(toolbar);
-        CollapsingToolbarLayout toolBarLayout = binding.toolbarLayout;
-        toolBarLayout.setTitle(getTitle());
+        postArrayList = new ArrayList<Post>();
 
-        FloatingActionButton fab = binding.fab;
-        fab.setOnClickListener(new View.OnClickListener() {
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseFirestore = FirebaseFirestore.getInstance();
+
+        getDataFromFirestore();
+
+
+        //RecyclerView
+
+        binding.recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        feedRecyclerAdapter = new FeedRecyclerAdapter(postArrayList);
+        binding.recyclerView.setAdapter(feedRecyclerAdapter);
+
+    }
+
+
+    public void getDataFromFirestore() {
+
+        CollectionReference collectionReference = firebaseFirestore.collection("Posts");
+
+        collectionReference.orderBy("date", Query.Direction.DESCENDING).addSnapshotListener(new EventListener<QuerySnapshot>() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException e) {
+
+                if (e != null) {
+                    Toast.makeText(FeedActivity.this,e.getLocalizedMessage().toString(),Toast.LENGTH_LONG).show();
+                }
+
+                if (queryDocumentSnapshots != null) {
+
+                    for (DocumentSnapshot snapshot : queryDocumentSnapshots.getDocuments()) {
+
+                        Map<String,Object> data = snapshot.getData();
+
+                        //Casting
+                        String comment = (String) data.get("comment");
+                        String userEmail = (String) data.get("useremail");
+                        String downloadUrl = (String) data.get("downloadurl");
+
+                        Post post = new Post(userEmail,comment,downloadUrl);
+
+                        postArrayList.add(post);
+
+                    }
+                    feedRecyclerAdapter.notifyDataSetChanged();
+
+                }
+
             }
         });
+
+
     }
+
+
 }
